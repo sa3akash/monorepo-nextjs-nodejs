72a916062f16a7faefb14e6c0a572045e63a670e
sa3akash <sa3akash@gmail.com>
2024-06-02 19:34:42 +0600

    setup nx

---
+++ apps/server/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { Server } from ".";
+
+class MainApp {
+  public runApp() {
+    const app = express();
+    const server: Server = new Server(app);
+    server.start();
+  }
+}
+
+const mainApp: MainApp = new MainApp();
+
+mainApp.runApp();
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,2 +1,27 @@
+import { Application } from "express";
+import cors from 'cors';
+import express from 'express';
 
-console.log('running server')
+
+export class Server {
+    private app: Application;
+    constructor(app: Application) {
+        this.app = app;
+    }
+
+    public start() {
+        this.app.listen(5500, () => {
+            console.log("Server is running on port 5500");
+        });
+        this.middleware(this.app);
+        this.routes(this.app);
+    }
+
+    private middleware(app: Application){
+        app.use(cors())
+        app.use(express.json())
+    }
+    private routes(app: Application){
+      
+    }
+}
